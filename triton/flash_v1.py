43a7251c6c3186396d4b35be5df7fd6ec5e142f6
szfmsmdx <1152631212@qq.com>
2026-01-22 01:16:11 +0800

    triton flash attn

--- triton/flash_v1.py
+++ triton/flash_v1.py
@@ -1,6 +1,7 @@
 import torch
 import triton
 import triton.language as tl
+from transformers.models.qwen3 import Qwen3Model
 
 def flash_attn_torch(
     q:torch.Tensor, k:torch.Tensor, v:torch.Tensor
@@ -9,4 +10,6 @@ def flash_attn_torch(
     q、k、v - 2D 矩阵
     """
     L, D = q.shape()
-    
+    for i in range(L):
+        for j in range(D):
+            qv, kv, vv = q[i,:], k.t()[:,j], v[i,:]
